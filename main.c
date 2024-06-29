93d493cdb00be8c30455956a287e010dfcc81047
Asfaq Moideen <asfaqmoideen@gmail.com>
2024-06-29 16:12:34 +0530

    feat: -v, --invert-match: Selects the non-matching lines of the provided input pattern. -n, --line-number: Prefix each line of the matching output with the line number in the input file.

--- main.c
+++ main.c
@@ -2,7 +2,12 @@
 #include <string.h>
 
 
-void searchForPattern(FILE *, char*);
+void searchPattern(FILE *, char*);
+void searchCaseInsensitivePattern(FILE* , char*);
+void findMatchedLineNumber(FILE*, char*);
+void printNonMatchingPattern(FILE*, char*);
+
+char* toLower(char*);
 
 int main(int args, char *argv[]){
     FILE *file;
@@ -13,13 +18,17 @@ int main(int args, char *argv[]){
         return 1;
     }    
 
-    searchForPattern(file, argv[1]);
+    searchPattern(file, argv[1]);
+    searchCaseInsensitivePattern(file, argv[1]);
+    findMatchedLineNumber(file,argv[1]);
+    printNonMatchingPattern(file, argv[1]);
+
     fclose(file);
     return 0;
 
 }
 
-void searchForPattern(FILE *file, char* pattern){
+void searchPattern(FILE *file, char* pattern){
 
     char line[256];
     while (fgets(line, sizeof(line), file)){
@@ -30,5 +39,51 @@ void searchForPattern(FILE *file, char* pattern){
             printf("%s\n", line);
         }
     }
+}
 
+void searchCaseInsensitivePattern(FILE *file, char* pattern){
+
+    char line[256];
+    while (fgets(line, sizeof(line), file)){
+
+        line[strcspn(line, "\n")] = '\0';
+
+        if(strstr(line, pattern) != NULL){
+            printf("%s\n", line);
+        }
+    }
+}
+
+void findMatchedLineNumber(FILE *file, char* pattern){
+    int lineNumber =0;
+    char line[256];
+    while (fgets(line, sizeof(line), file)){
+
+        line[strcspn(line, "\n")] = '\0';
+
+        if(strstr(line, pattern) != NULL){
+            printf("%d",lineNumber++);
+        }
+    }
+}
+
+void printNonMatchingPattern(FILE *file, char* pattern){
+
+    char line[256];
+    while (fgets(line, sizeof(line), file)){
+
+        line[strcspn(line, "\n")] = '\0';
+
+        if(strstr(line, pattern) == NULL){
+            printf("%s\n", line);
+        }
+    }
+}
+
+char* toLower(char* pattern){
+    char* loweredChar; 
+    for(int i=0; i<sizeof(pattern); i++){
+        if (pattern[i] < 125 )
+        return NULL;
+    }
 }
